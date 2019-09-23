506e1d87184546fc9fbb639090591478ca78ac39
Xiang Gao <gxiang1228@gmail.com>
2019-09-23 09:28:07 -0700

    Update arxiv.py

--- data/arXiv/arxiv.py
+++ data/arXiv/arxiv.py
@@ -7,6 +7,43 @@ EQN_token = '_eqn_'
 CITE_token = '_cite_'
 IX_token = '_ix_'
 
+import re
+
+def norm_sentence(txt):
+	txt = txt.lower()
+
+	# url and tag
+	words = []
+	for word in txt.lower().split():
+		if word[0] == '#':	# don't allow tag
+			continue
+		i = word.find('http') 
+		if i >= 0:
+			word = word[:i] + ' ' + '__url__'
+		words.append(word.strip())
+	txt = ' '.join(words)
+
+	# remove illegal char
+	txt = txt.replace(chr(92),'')	# chr(92) = '\'. as twitter has 'b\/c' rather than 'b/c'
+	txt = txt.replace("b/c","because").replace('j/k','just kidding').replace('w/o','without').replace('w/','with')
+	txt = re.sub('__mention__','MENTION',txt)
+	txt = re.sub('__url__','URL',txt)
+	txt = re.sub(r"[^A-Za-z0-9():,.!?_'“” ]", " ", txt)
+	txt = re.sub('MENTION','__mention__',txt)	
+	txt = re.sub('URL','__url__',txt)	
+
+	# contraction
+	add_space = ["'s", "'m", "'re", "n't", "'ll","'ve","'d","'em"]
+	tokenizer = TweetTokenizer(preserve_case=False)
+	txt = ' ' + ' '.join(tokenizer.tokenize(txt)) + ' '
+	txt = txt.replace(" won't ", " will n't ")
+	txt = txt.replace(" can't ", " can n't ")
+	for a in add_space:
+		txt = txt.replace(a+' ', ' '+a+' ')
+	
+	# remove un-necessary space
+	return ' '.join(txt.split())
+
 def arxiv_del_bib(path):
 	lines = []
 	for line in open(path, encoding='utf-8', errors='ignore'):
